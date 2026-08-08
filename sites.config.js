eb349c4f2097a54fe007dd54a9f332fa6d24f35c
Armond452Alt <trairsp@gmail.com>
2026-08-07 21:32:14 -0400

    Refactor sites.config.js to use ES module syntax

--- sites.config.js
+++ sites.config.js
@@ -1,22 +1,4 @@
-module.exports = {
+export default {
   output: 'CN/AS.xml',
-  sites: [
-    {
-      site: 'tvpassport.com',
-      channels: [
-        {
-          lang: 'en',
-          site_id: 'tv-listings/stations/cartoon-network-usa-east/237',
-          xmltv_id: 'CartoonNetworkEast.us',
-          name: 'Cartoon Network East'
-        },
-        {
-          lang: 'en',
-          site_id: 'tv-listings/stations/adult-swim-east/3089',
-          xmltv_id: 'AdultSwimEast.us',
-          name: 'Adult Swim East'
-        }
-      ]
-    }
-  ]
-};
+  channels: 'CN/AS.channels.xml'
+}
