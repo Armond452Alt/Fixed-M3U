e60bf6d283eed22e93cf1320c6371c7b1737e5f4
Armond452Alt <trairsp@gmail.com>
2026-08-07 21:07:57 -0400

    Add sites configuration for TV listings

---
+++ sites.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  output: 'CN/AS.xml',
+  sites: [
+    {
+      site: 'tvpassport.com',
+      channels: [
+        {
+          lang: 'en',
+          site_id: 'tv-listings/stations/cartoon-network-usa-east/237',
+          xmltv_id: 'CartoonNetworkEast.us',
+          name: 'Cartoon Network East'
+        },
+        {
+          lang: 'en',
+          site_id: 'tv-listings/stations/adult-swim-east/3089',
+          xmltv_id: 'AdultSwimEast.us',
+          name: 'Adult Swim East'
+        }
+      ]
+    }
+  ]
+};
