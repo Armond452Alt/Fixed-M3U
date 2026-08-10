6d052ba1be860c803a71887e8b4db06bcefc06da
Armond452Alt <trairsp@gmail.com>
2026-08-10 05:41:48 -0400

    Refactor channel definitions to use Channel class

--- grab.js
+++ grab.js
@@ -1,27 +1,26 @@
-import grabber from 'epg-grabber'
+import { grabber, Channel } from 'epg-grabber'
 import fs from 'fs'
 
 const channels = [
-  {
+  new Channel({
     site: 'tvpassport.com',
     site_id: 'tv-listings/stations/cartoon-network-usa-east/237',
     xmltv_id: 'CartoonNetworkEast.us',
     name: 'Cartoon Network East',
     lang: 'en'
-  },
-  {
+  }),
+  new Channel({
     site: 'tvpassport.com',
     site_id: 'tv-listings/stations/adult-swim-east/3089',
     xmltv_id: 'AdultSwimEast.us',
     name: 'Adult Swim East',
     lang: 'en'
-  }
+  })
 ]
 
 async function run() {
   console.log('Fetching EPG data...')
   
-  // High-level grabber method handles Channel instantiation internally
   const xml = await grabber.grab({ channels })
   
   fs.writeFileSync('CN/AS.xml', xml)
