7d036155398998197d76a18a39e56aea73e9bb91
Armond452Alt <trairsp@gmail.com>
2026-08-10 05:45:31 -0400

    Refactor grab.js to use EPGGrabber class

--- grab.js
+++ grab.js
@@ -1,28 +1,41 @@
-import { grabber, Channel } from 'epg-grabber'
+import { EPGGrabber } from 'epg-grabber'
 import fs from 'fs'
 
+const grabber = new EPGGrabber()
+
 const channels = [
-  new Channel({
+  {
     site: 'tvpassport.com',
     site_id: 'tv-listings/stations/cartoon-network-usa-east/237',
     xmltv_id: 'CartoonNetworkEast.us',
     name: 'Cartoon Network East',
     lang: 'en'
-  }),
-  new Channel({
+  },
+  {
     site: 'tvpassport.com',
     site_id: 'tv-listings/stations/adult-swim-east/3089',
     xmltv_id: 'AdultSwimEast.us',
     name: 'Adult Swim East',
     lang: 'en'
-  })
+  }
 ]
 
 async function run() {
   console.log('Fetching EPG data...')
   
-  const xml = await grabber.grab({ channels })
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n<tv>'
   
+  for (const channel of channels) {
+    console.log(`Grabbing ${channel.name}...`)
+    const result = await grabber.fetch(channel)
+    if (result && result.xml) {
+      xml += result.xml
+    }
+  }
+  
+  xml += '\n</tv>'
+  
+  fs.mkdirSync('CN', { recursive: true })
   fs.writeFileSync('CN/AS.xml', xml)
   console.log('CN/AS.xml successfully updated!')
 }
