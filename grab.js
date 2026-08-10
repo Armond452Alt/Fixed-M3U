db8d7854ff9781b71293691f9417ef8f3a6d2e80
Armond452Alt <trairsp@gmail.com>
2026-08-10 05:39:26 -0400

    Refactor grab.js to use new grabber API

--- grab.js
+++ grab.js
@@ -1,29 +1,29 @@
-import { EPGGrabber, Channel } from 'epg-grabber'
+import grabber from 'epg-grabber'
 import fs from 'fs'
 
-const grabber = new EPGGrabber()
-
 const channels = [
-  new Channel({
+  {
     site: 'tvpassport.com',
     site_id: 'tv-listings/stations/cartoon-network-usa-east/237',
     xmltv_id: 'CartoonNetworkEast.us',
     name: 'Cartoon Network East',
     lang: 'en'
-  }),
-  new Channel({
+  },
+  {
     site: 'tvpassport.com',
     site_id: 'tv-listings/stations/adult-swim-east/3089',
     xmltv_id: 'AdultSwimEast.us',
     name: 'Adult Swim East',
     lang: 'en'
-  })
+  }
 ]
 
 async function run() {
   console.log('Fetching EPG data...')
-  // Pass channels directly as an array argument
-  const xml = await grabber.grab(channels)
+  
+  // High-level grabber method handles Channel instantiation internally
+  const xml = await grabber.grab({ channels })
+  
   fs.writeFileSync('CN/AS.xml', xml)
   console.log('CN/AS.xml successfully updated!')
 }
